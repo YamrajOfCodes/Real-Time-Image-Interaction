e3fe4c54161c2fb10da5e0e32a2adc62cd1c1e35
YamrajOfcodes <kundanpatil0111@.gmail.com>
2026-01-05 13:32:38 +0530

    dbChanges

--- src/lib/InstantDbConnect.js
+++ src/lib/InstantDbConnect.js
@@ -1,18 +1,20 @@
 import { init } from "@instantdb/react"; // Changed from @instantdb/core
 import { i } from "@instantdb/core";
 
-const APP_ID = "aef4df1b-447c-49c0-ba91-de4f8237a3d0";
+const APP_ID = "a96d6b07-fac9-437d-8fbc-e08adb3222f7";
 
 export const schema = i.schema({
   entities: {
     
     reactions: i.entity({
       imageId: i.string(),
+      userName:i.string(),
       emojis: i.string(),
       userId: i.string(),
       createdAt: i.number(),
     }),
     comments: i.entity({
+      userName:i.string(),
       imageId: i.string(),
       text: i.string(),
       userId: i.string(),
